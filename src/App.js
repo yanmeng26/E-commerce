40b666f8ec35b9a58696b006503f1652822605aa
yanmeng26 <yanme@oregonstate.edu>
2019-12-29 23:18:54 -0700

    update header component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import HomePage from './pages/homepage/homepage.component';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
 import ShopPage from'./pages/shop/shop.jsx';
 import './App.css';
+import Header from './components/header/header.jsx';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Header/>
       <Switch>
         <Route exact path ='/' component = {HomePage}/>
         <Route exact path ='/shop' component = {ShopPage}/>
