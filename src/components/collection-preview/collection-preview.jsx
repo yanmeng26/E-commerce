aafddb4065b0e78a20d4d635fd9a26fdb1b36870
yanmeng26 <yanme@oregonstate.edu>
2020-01-02 13:05:32 -0700

    update shop data with new data, update cart reducer

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -3,25 +3,16 @@ import './collection-preview-style.scss';
 import CollectionItem from "../collection-item/collection-item.component";
 
 
-const CollectionPreview =({title, items})=>(
-
-    <div className = 'collection-preview'>
-        <h1 className = "title">{title.toUpperCase()}</h1>
-        <div className = "preview">
-        {
-            items
-            .filter((item,idx)=>idx<4)
-            .map(({ id, ...otherItemProps }) => (
-                <CollectionItem key={id} {...otherItemProps} />
-              ))
-            
-        }
-        </div>
-    
-
+const CollectionPreview = ({ title, items }) => (
+    <div className='collection-preview'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <div className='preview'>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
-
-
-);
-
+  );
 export default CollectionPreview;
