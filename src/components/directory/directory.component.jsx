f676028ed9ad93a7db3715061949dca5b7910674
yanmeng26 <yanme@oregonstate.edu>
2020-01-02 22:03:02 -0700

    add strip payment component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,60 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
+import { createStructuredSelector } from 'reselect';
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-    this.state = {
-      sections: [
-        {
-          title: 'bags',
-          imageUrl: `${window.location.origin}` + "/img/bags.png",
-          id: 1,
-          linkUrl: 'bags'
-        },
-        {
-          title: 'T-Shirts',
-          imageUrl: `${window.location.origin}` + "/img/sweaters.png",
-          id: 2,
-          linkUrl:'shirts'
-        },
-        {
-          title: 'accessories',
-          imageUrl: `${window.location.origin}` + "/img/accessories.png",
-          id: 3,
-          linkUrl:'accessories'
-        },
-        {
-          title: 'women',
-          imageUrl: `${window.location.origin}` + "/img/women.png",
-          size: 'large',
-          id: 4,
-          linkUrl:'women'
-        },
-        {
-          title: 'men',
-          imageUrl: `${window.location.origin}` + "/img/men.png",
-          size: 'large',
-          id: 5,
-          linkUrl:'men'
-        }
-      ]
-    };
-  }
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size,linkUrl }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_qtkt6OR4pQRHmCzbgEMvJyeW00ZhzAa6ce';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Meng Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image={window.location.origin + `/img/logo.png`}
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
