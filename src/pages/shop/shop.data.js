c1e6c807a3b32c2ce0dfa22ab00e86fe2d4780ff
yanmeng26 <yanme@oregonstate.edu>
2020-02-12 22:51:02 -0700

    change shop data

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,249 +1,273 @@
-const SHOP_DATA = [
-  {
+const SHOP_DATA = {
+  vegan: {
     id: 1,
     title: 'Vegan',
     routeName: 'vegan',
     items: [
       {
         id: 1,
-        name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+        name: 'Broccoli',
+        imageUrl: 'https://i.ibb.co/QYkGvWv/foodism360-Gwak7-T9ev-Ek-unsplash.jpg',
         price: 25
       },
       {
         id: 2,
-        name: 'Blue Beanie',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        name: 'Potato',
+        imageUrl: 'https://i.ibb.co/FVJcMC8/nicolas-dmitrichev-7-P81m-Yhi-Vp4-unsplash.jpg',
         price: 18
       },
       {
         id: 3,
-        name: 'Brown Cowboy',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        price: 35
+        name: 'Tomato',
+        imageUrl: 'https://i.ibb.co/4NVT88L/maja-petric-E4-Xps-Df-Nrh4-unsplash.jpg',
+        price: 13
       },
       {
         id: 4,
-        name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+        name: 'Mushroom',
+        imageUrl: 'https://i.ibb.co/bFbRNP5/christine-siracusa-XJY1-C5-LVNn8-unsplash.jpg',
         price: 25
       },
       {
         id: 5,
-        name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+        name: 'Vega-Combo-1',
+        imageUrl: 'https://i.ibb.co/CVZPp1q/heather-ford-Ug7kk0k-Th-Lk-unsplash.jpg',
         price: 18
       },
       {
         id: 6,
-        name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+        name: 'Vega-Combo-3',
+        imageUrl: 'https://i.ibb.co/YfVJxcx/rebeca-g-sendroiu-Et-ANw-Y5-TXJY-unsplash.jpg',
         price: 14
       },
       {
         id: 7,
-        name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+        name: 'Vega-Combo -2',
+        imageUrl: 'https://i.ibb.co/PZtWykw/eiliv-sonas-aceron-319-ROaclyg-A-unsplash.jpg',
         price: 18
       },
       {
         id: 8,
-        name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+        name: 'Vega-Combo-4',
+        imageUrl: 'https://i.ibb.co/R7X07tN/nadine-primeau-ft-Wfohtj-Nw-unsplash.jpg',
         price: 14
       },
       {
         id: 9,
-        name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+        name: 'Vega-Combo-5',
+        imageUrl: 'https://i.ibb.co/3kSC1b8/monika-grabkowska-ef-Cc8-CPwh-M-unsplash.jpg',
         price: 16
       }
     ]
   },
-  {
+  cereals: {
     id: 2,
-    title: 'Cerealss',
+    title: 'Cereals',
     routeName: 'cereals',
     items: [
       {
         id: 10,
-        name: 'Adidas NMD',
-        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-        price: 220
+        name: 'Cereal with Strawberry',
+        imageUrl: 'https://i.ibb.co/kxzGBkL/dani-rendina-SH6vc3-VOOw-E-unsplash.jpg',
+        price: 22
       },
       {
         id: 11,
-        name: 'Adidas Yeezy',
-        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-        price: 280
+        name: 'Cereal with Blueberry',
+        imageUrl: 'https://i.ibb.co/8KCYF81/alison-marras-QSvv6syq-NUw-unsplash.jpg',
+        price: 12
       },
       {
         id: 12,
-        name: 'Black Converse',
-        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-        price: 110
+        name: 'Cereal with PineApple',
+        imageUrl: 'https://i.ibb.co/rMw3PB0/kevin-mccutcheon-8q-FPu-K-t-Bu-Y-unsplash.jpg',
+        price: 11
       },
       {
         id: 13,
-        name: 'Nike White AirForce',
-        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-        price: 160
+        name: 'Cereal with Peach',
+        imageUrl: 'https://i.ibb.co/PwgYRGH/alexander-mils-Dg2mq-Tiq-Ic-unsplash.jpg',
+        price: 16
       },
       {
         id: 14,
-        name: 'Nike Red High Tops',
-        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-        price: 160
+        name: 'Cereal with Banana',
+        imageUrl: 'https://i.ibb.co/XWtY0Hk/federica-diliberto-i2e-Yy-Fe-Tfwc-unsplash.jpg',
+        price: 16
       },
       {
         id: 15,
-        name: 'Nike Brown High Tops',
-        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-        price: 160
+        name: 'Cereal Combo 1',
+        imageUrl: 'https://i.ibb.co/T8FDnMS/alice-pasqual-7ej-Ds-Ms-Af-M-unsplash.jpg',
+        price: 16
       },
       {
         id: 16,
-        name: 'Air Jordan Limited',
-        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-        price: 190
+        name: 'Pure Cereal',
+        imageUrl: 'https://i.ibb.co/t3ppPHr/calum-lewis-KMx5-Yilip-X0-unsplash-1.jpg',
+        price: 19
       },
       {
         id: 17,
-        name: 'Timberlands',
-        imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-        price: 200
+        name: 'Cereal with Nuts',
+        imageUrl: 'https://i.ibb.co/n1msCJj/milada-vigerova-Po-GXTT4mcx8-unsplash.jpg',
+        price: 20
       }
     ]
   },
-  {
+  fruits: {
     id: 3,
     title: 'Fruits',
-    routeName: 'Fruits',
+    routeName: 'fruits',
     items: [
       {
-        id: 18,
-        name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-        price: 125
+        id: 1,
+        name: 'BlueBerry',
+        imageUrl: 'https://i.ibb.co/SxQM6ZN/aliona-gumeniuk-Y9-WTwredge0-unsplash.jpg',
+        price: 25
       },
       {
-        id: 19,
-        name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-        price: 90
+        id: 2,
+        name: 'Banana',
+        imageUrl: 'https://i.ibb.co/hB9kKW7/charles-0v-1-TPz1u-Xw-unsplash.jpg',
+        price: 18
       },
       {
-        id: 20,
-        name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-        price: 90
+        id: 3,
+        name: 'Apple',
+        imageUrl: 'https://i.ibb.co/tp3YvhK/elena-koycheva-u-Mj-XPQRMm8-A-unsplash.jpg',
+        price: 13
       },
       {
-        id: 21,
-        name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-        price: 165
+        id: 4,
+        name: 'Strawberry',
+        imageUrl: 'https://i.ibb.co/ZKRwmWZ/lewis-fagg-CE1yaljvkb0-unsplash.jpg',
+        price: 25
       },
       {
-        id: 22,
-        name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-        price: 185
+        id: 5,
+        name: 'Avacado',
+        imageUrl: 'https://i.ibb.co/W2B9NxK/irene-kredenets-AWMWc-R3h-QUg-unsplash.jpg',
+        price: 18
+      },
+      {
+        id: 6,
+        name: 'Orange ',
+        imageUrl: 'https://i.ibb.co/1nRFyJZ/xiaolong-wong-5-DTM9y7v-Up0-unsplash-1.jpg',
+        price: 14
+      },
+      {
+        id: 7,
+        name: 'Combo -1 ',
+        imageUrl: 'https://i.ibb.co/Xxzm98B/armin-djuhic-XMHIij-BZCJo-unsplash.jpg',
+        price: 18
+      },
+      {
+        id: 8,
+        name: 'Combo-2',
+        imageUrl: 'https://i.ibb.co/GdHhnp0/rayia-soderberg-qy5-Dkk-RIQj-U-unsplash.jpg',
+        price: 14
+      },
+      {
+        id: 9,
+        name: 'Combo-3',
+        imageUrl: 'https://i.ibb.co/nPZJg2V/yoksel-zok-d-Pe-RZs-Hzmj-E-unsplash.jpg',
+        price: 16
       }
     ]
   },
-  {
+  womens: {
     id: 4,
     title: 'Womens',
     routeName: 'womens',
     items: [
       {
         id: 23,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+        name: 'Woman-combo-1',
+        imageUrl: 'https://i.ibb.co/QJFqrWm/ana-azevedo-xzq-QRay-Yd-VI-unsplash.jpg',
         price: 25
       },
       {
         id: 24,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+        name: 'Woman-combo-2',
+        imageUrl: 'https://i.ibb.co/pzkt6HY/tim-chow-Mhpx-Hv0-FKt-E-unsplash.jpg',
         price: 20
       },
       {
         id: 25,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 80
+        name: 'Woman-combo-3',
+        imageUrl: 'https://i.ibb.co/r6bGr4j/eve-lina-wfp2-U1-Rb-Z8-A-unsplash.jpg',
+        price: 18
       },
       {
         id: 26,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 80
+        name: 'Woman-combo-4',
+        imageUrl: 'https://i.ibb.co/N7hQCdr/emilia-wronowska-Zo-1-Pp1jd8-Y-unsplash.jpg',
+        price: 18
       },
       {
         id: 27,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-        price: 45
+        name: 'Woman-cereal',
+        imageUrl: 'https://i.ibb.co/jT2mKxX/yvens-banatte-j-Mryv3-G7sx-Y-unsplash.jpg',
+        price: 24
       },
       {
         id: 28,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-        price: 135
+        name: 'Woman-combo-5',
+        imageUrl: 'https://i.ibb.co/mvMQ6dS/brooke-lark-oaz0rays-ASk-unsplash-1.jpg',
+        price: 13
       },
       {
         id: 29,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
+        name: 'Woman-combo-6',
+        imageUrl: 'https://i.ibb.co/P5DmbR6/alison-marras-El-Gnfom4t-Iw-unsplash.jpg',
         price: 20
       }
     ]
   },
-  {
+  mens: {
     id: 5,
     title: 'Mens',
     routeName: 'mens',
     items: [
       {
         id: 30,
-        name: 'Camo Down Vest',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-        price: 325
+        name: 'Men-combo-1',
+        imageUrl: 'https://i.ibb.co/Cv4X09x/eiliv-sonas-aceron-An6c-Tgm-C8yk-unsplash.jpg',
+        price: 25
       },
       {
         id: 31,
-        name: 'Floral T-shirt',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+        name: 'Men-combo-2',
+        imageUrl: 'https://i.ibb.co/mFJVXns/tetiana-bykovets-T4-Q2-NX1-GP4w-unsplash.jpg',
         price: 20
       },
       {
         id: 32,
-        name: 'Black & White Longsleeve',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+        name: 'Men-combo-3',
+        imageUrl: 'https://i.ibb.co/wy47YTn/joseph-gonzalez-Qa-GDmf5t-Mi-E-unsplash.jpg',
         price: 25
       },
       {
         id: 33,
-        name: 'Pink T-shirt',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+        name: 'Men-combo-4',
+        imageUrl: 'https://i.ibb.co/tsgFMP5/eiliv-sonas-aceron-shmr-Ko-BQXQ-unsplash.jpg',
         price: 25
       },
       {
         id: 34,
-        name: 'Jean Long Sleeve',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        price: 40
+        name: 'Men-combo-5',
+        imageUrl: 'https://i.ibb.co/55V0x5H/eiliv-sonas-aceron-99mu-SP3cqh-Y-unsplash.jpg',
+        price: 14
       },
       {
         id: 35,
-        name: 'Burgundy T-shirt',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+        name: 'Men-combo-6',
+        imageUrl: 'https://i.ibb.co/ZXPLChC/john-fornander-f6-Z44o6-ESCI-unsplash.jpg',
         price: 25
       }
     ]
   }
-];
+};
 
 export default SHOP_DATA;
